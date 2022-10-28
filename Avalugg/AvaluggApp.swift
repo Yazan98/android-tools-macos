0e99370f8357a1abf8f64517af09d65202ace0a6
Yazan Tarifi <yazantarifi989@gmail.com>
2022-10-28 09:11:01 +0300

    Build Project Files

---
+++ Avalugg/AvaluggApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvaluggApp.swift
+//  Avalugg
+//
+//  Created by Yazan Tarifi on 28/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AvaluggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
